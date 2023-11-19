3ba600bc2c0bb1a32d4b8a9f223da206dc022f22
EvgeniyZo <evgeniizo@yandex.ru>
2023-11-19 15:09:54 +0300

    api

--- src/backend/app/app/main.py
+++ src/backend/app/app/main.py
@@ -1,12 +1,13 @@
 
 
-
+'''
 import databases
 import ormar
 import sqlalchemy
 import uvicorn
+'''
 
-from sqlalchemy import Column, String, Float, Integer
+from sqlalchemy import Column, String, Float, Integer, Boolean
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
@@ -17,12 +18,10 @@ from typing import Union
 from pydantic import BaseModel
 from fastapi import FastAPI, APIRouter
 
-from fastapi_crudrouter import OrmarCRUDRouter as CRUDRouter # SQLAlchemyCRUDRouter as CRUDRouter
+from fastapi_crudrouter import SQLAlchemyCRUDRouter as CRUDRouter # SQLAlchemyCRUDRouter as CRUDRouter
 
 
-DATABASE_URL = "sqlite:///./test.db"
-database = databases.Database(DATABASE_URL)
-metadata = sqlalchemy.MetaData()
+
 
 
 
@@ -30,128 +29,223 @@ app = FastAPI(
     title="tenderhack noname", openapi_url=f"/openapi.json" # settings.PROJECT_NAME # settings.API_V1_STR
 )
 
-@app.on_event("startup")
-async def startup():
-    await database.connect()
-
-@app.on_event("shutdown")
-async def shutdown():
-    await database.disconnect()
-
-class BaseMeta(ormar.ModelMeta):
-    metadata = metadata
-    database = database
-
-def _setup_database():
-    # if you do not have the database run this once
-    engine = sqlalchemy.create_engine(DATABASE_URL)
-    metadata.drop_all(engine)
-    metadata.create_all(engine)
-    return engine, database
-
-
-
-
-
 
 engine = create_engine(
+    #"postgresql://ezz:ezz@localhost:5432/ezz"
     "sqlite:///./app.db",
     connect_args={"check_same_thread": False}
 )
 
+SessionLocal = sessionmaker(
+    autocommit=False,
+    autoflush=False,
+    bind=engine
+)
 
-class Users(ormar.Model):
-    class Meta(BaseMeta):
-        pass
-    id: ormar.Integer(primary_key=True)
-    name: ormar.String(max_length=255)
-    inn: ormar.Integer()
-    kpp: ormar.Integer()
-
-class Items(ormar.Model):
-    class Meta(BaseMeta):
-        pass
-    id: ormar.Integer(primary_key=True)
-    name: ormar.String(max_length=255)
-
-class KSessions(ormar.Model): #Participants
-    class Meta(BaseMeta):
-        pass
-    id: ormar.Integer(primary_key=True)
-    Participant_inn: ormar.ForeignKey(Users, related_name="sessions")
-    Participant_kpp: ormar.Integer()
-    Is_winner: ormar.Integer()
-    Id_ks: ormar.Integer()
-    Publish_date: ormar.Integer()
-    Price: ormar.Integer()
-    Customer_inn: ormar.Integer()
-    Customer_kpp: ormar.Integer()
-    Kpgs: ormar.Integer()
-    name: ormar.Integer()
-    Items: ormar.Integer()
-    Region_code: ormar.Integer()
-    Violations: ormar.Boolean()
+Base = declarative_base()
 
 
-class Contracts(ormar.Model):
-    class Meta(BaseMeta):
-        pass
-    id: ormar.Integer(primary_key=True)
-    Id_ks: ormar.Integer() # fk KSessions id
-    Contract_id: ormar.Integer()
-    Conclusion_date: ormar.Integer()
-    Price: ormar.Integer()
-    Customer_inn: ormar.Integer()
-    Customer_kpp: ormar.Integer()
-    Supplier_inn: ormar.Integer()
-    Supplier_kpp: ormar.Integer()
-    Violations: ormar.Integer()
-    Status: ormar.Integer()
-
-class Blocking(ormar.Model):
-    class Meta(BaseMeta):
-        pass
-    id: ormar.Integer(primary_key=True)
-    Supplier_inn: ormar.Integer()
-    Supplier_kpp: ormar.Integer()
-    Reason: ormar.Integer()
-    Blocking_start_date: ormar.Integer()
-    Blocking_end_date: ormar.Integer()
-
-class Contract_execution(ormar.Model):
-    class Meta(BaseMeta):
-        pass
-    id: ormar.Integer(primary_key=True)
-    Contract_id: ormar.Integer()
-    Upd_id: ormar.Integer()
-    Scheduled_delivery_date: ormar.Integer()
-    Actual_delivery_date: ormar.Integer()
-    Supplier_inn: ormar.Integer()
-    Supplier_kpp: ormar.Integer()
-    Customer_inn: ormar.Integer()
-    Customer_kpp: ormar.Integer()
+def get_db():
+    session = SessionLocal()
+    try:
+        yield session
+        session.commit()
+    finally:
+        session.close()
 
 
 
 
 
 
+
+
+
+class Users(BaseModel):
+    
+    id: int
+    name: str
+    inn: int
+    kpp: int
+
+    class Config:
+        orm_mode = True
+#Users.update_forward_refs()
+
+class UsersModel(Base):
+    __tablename__ = 'users'
+    id = Column(Integer, primary_key=True, index=True)
+    name = Column(String)
+    inn = Column(Integer)
+    kpp = Column(Integer)
+
+
+
+
+class Items(BaseModel):
+    id: int
+    name: str
+
+    class Config:
+        orm_mode = True
+
+class ItemsModel(Base):
+    __tablename__ = 'items'
+    id = Column(Integer, primary_key=True, index=True)
+    name = Column(String)
+
+class KSessions(BaseModel): #Participants
+    id: int
+    participant_inn: int
+    participant_kpp: int
+    is_winner: int
+    id_ks: int
+    publish_date: int
+    price: int
+    customer_inn: int
+    customer_kpp: int
+    kpgs: int
+    name: int
+    items: int
+    region_code: int
+    violations: int
+
+    class Config:
+        orm_mode = True
+
+class KSessionsModel(Base):
+    __tablename__ = 'ksessions'
+    id = Column(Integer, primary_key=True, index=True)
+    participant_inn: Column(Integer) #ormar.ForeignKey(Users, related_name="sessions")
+    participant_kpp: Column(Integer)
+    is_winner: Column(Integer)
+    id_ks: Column(Integer)
+    publish_date: Column(Integer)
+    price: Column(Integer)
+    customer_inn: Column(Integer)
+    customer_kpp: Column(Integer)
+    kpgs: Column(Integer)
+    name: Column(Integer)
+    items: Column(Integer)
+    region_code: Column(Integer)
+    violations: Column(Boolean)
+
+class Contracts(BaseModel):
+    id: int
+    Id_ks: int # fk KSessions id
+    Contract_id: int
+    Conclusion_date: int
+    Price: int
+    Customer_inn: int
+    Customer_kpp: int
+    Supplier_inn: int
+    Supplier_kpp: int
+    Violations: int
+    Status: int
+
+    class Config:
+        orm_mode = True
+
+class ContractsModel(Base):
+    __tablename__ = 'contracts'
+    id = Column(Integer, primary_key=True, index=True)
+    Id_ks: Column(Integer) # fk KSessions id
+    Contract_id: Column(Integer)
+    Conclusion_date: Column(Integer)
+    Price: Column(Integer)
+    Customer_inn: Column(Integer)
+    Customer_kpp: Column(Integer)
+    Supplier_inn: Column(Integer)
+    Supplier_kpp: Column(Integer)
+    Violations: Column(Integer)
+    Status: Column(Integer)
+
+class Blocking(BaseModel):
+    id: int
+    Supplier_inn: int
+    Supplier_kpp: int
+    Reason: int
+    Blocking_start_date: int
+    Blocking_end_date: int
+
+    class Config:
+        orm_mode = True
+
+class BlockingModel(Base):
+    __tablename__ = 'blocking'
+    id = Column(Integer, primary_key=True, index=True)
+    Supplier_inn: Column(Integer)
+    Supplier_kpp: Column(Integer)
+    Reason: Column(Integer)
+    Blocking_start_date: Column(Integer)
+    Blocking_end_date: Column(Integer)
+
+
+
+class Contract_executionCreate(BaseModel):
+    Contract_id: int
+    Upd_id: int
+    Scheduled_delivery_date: int
+    Actual_delivery_date: int
+    Supplier_inn: int
+    Supplier_kpp: int
+    Customer_inn: int
+    Customer_kpp: int
+
+class Contract_execution(Contract_executionCreate):
+    id: int
+
+    class Config:
+        orm_mode = True
+
+class Contract_executionModel(Base):
+    __tablename__ = 'contract_execution'
+    id = Column(Integer, primary_key=True, index=True)
+    Contract_id: Column(Integer)
+    Upd_id: Column(Integer)
+    Scheduled_delivery_date: Column(Integer)
+    Actual_delivery_date: Column(Integer)
+    Supplier_inn: Column(Integer)
+    Supplier_kpp: Column(Integer)
+    Customer_inn: Column(Integer)
+    Customer_kpp: Column(Integer)
+
+
+
+
+
+Base.metadata.create_all(bind=engine)
 
 
 
 
 #app.include_router(APIRouter())
 
-app.include_router(CRUDRouter(schema=KSessions, paginate=20))
 
-app.include_router(CRUDRouter(schema=Contracts, paginate=20))
 
-app.include_router(CRUDRouter(schema=Blocking, paginate=20))
+app.include_router(CRUDRouter(schema=Users,db_model=UsersModel,
+    db=get_db,
+    prefix='users', paginate=20))
+app.include_router(CRUDRouter(schema=Items,db_model=ItemsModel,
+    db=get_db,
+    prefix='items', paginate=20))
 
-app.include_router(CRUDRouter(schema=Contract_execution, paginate=20))
 
-app.include_router(CRUDRouter(schema=Users, paginate=20))
-app.include_router(CRUDRouter(schema=Items, paginate=20))
+app.include_router(CRUDRouter(schema=KSessions,db_model=KSessionsModel,
+    db=get_db,
+    prefix='ksessions', paginate=20))
+
+app.include_router(CRUDRouter(schema=Contracts,db_model=ContractsModel,
+    db=get_db,
+    prefix='contracts', paginate=20))
+
+app.include_router(CRUDRouter(schema=Blocking,db_model=BlockingModel,
+    db=get_db,
+    prefix='blocking', paginate=20))
+
+app.include_router(CRUDRouter(schema=Contract_execution,create_schema=Contract_executionCreate,db_model=Contract_executionModel,
+    db=get_db,
+    prefix='contract_execution', paginate=20))
 
 
 
@@ -166,6 +260,7 @@ def read_item(test_item_id: int, q: Union[str, None] = None):
     return {"test_item_id": test_item_id, "q": q}
 
 
-
+#if __name__ == "__main__":
+#    uvicorn.run("main:app", host="127.0.0.1", port=5000, log_level="info")
 
 
